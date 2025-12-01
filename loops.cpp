07046a541edc075bc0a4ea21d18886915c3083c0
jenae-ms <jenae.sbraga@icloud.com>
2025-12-01 13:28:54 -0600

    i just editied this

--- loops.cpp
+++ loops.cpp
@@ -2,7 +2,13 @@
 using namespace std;
 int main(){
 
-cout<<"Hi";
+string name;
+
+cout<"Hello there!"<<endl;
+cout<<"What is your name? "<<endl;
+cin>>name;
+cout<<" "<<endl;
+cout<<name<<" is a beautiful name!"<<endl;
 
 return 0;
 }
